32a13436f296a0ae72290289152daa7b7abb254a
Eduard Maghakyan <edi.maghakyan@gmail.com>
2017-08-19 16:28:42 +0200

    Added submit button and unit tests

--- src/components/addTodo/AddTodo.test.js
+++ src/components/addTodo/AddTodo.test.js
@@ -1,11 +1,37 @@
-/* global browser, describe, it */
+/* global describe,it,expect,jest,beforeEach */
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import AddTodo from '.';
 
 describe('AddTodo Component', () => {
+  let component;
+  const submitMock = jest.fn();
+
+  beforeEach(() => {
+    component = shallow(
+      <AddTodo
+        submitTodo={submitMock}
+      />,
+    );
+  });
+
   it('Should render component', () => {
-    const component = shallow(<AddTodo/>);
     expect(component.exists()).toEqual(true);
+  });
+
+  it('Should render on input', () => {
+    expect(component.find('.todo-input').length).toEqual(1);
+  });
+
+  it('Should add a submit button', () => {
+    expect(component.find('.todo-submit').length).toEqual(1);
+  });
+
+  it('Should call submitTodo function', () => {
+    component = mount(<AddTodo submitTodo={submitMock} />);
+
+    expect(submitMock.mock.calls.length).toEqual(0);
+    component.find('form').simulate('submit');
+    expect(submitMock.mock.calls.length).toEqual(1);
   });
 });
