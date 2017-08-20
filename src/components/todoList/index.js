995d9fc32ab5883008f389e6b21f91f9140d69d3
Eduard Maghakyan <edi.maghakyan@gmail.com>
2017-08-20 20:19:25 +0200

    Removed unneeded console

--- src/components/todoList/index.js
+++ src/components/todoList/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const TodoList = ({ todos }) => {
-  console.log(todos);
   const todoItems = todos.map(todo => (
     <li key={todo.id}>
       <span className="todo-text">{todo.text}</span>
