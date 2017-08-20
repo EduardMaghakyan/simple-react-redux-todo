d90a02aa498f9428010cc8ff184e52285c6e7297
Eduard Maghakyan <edi.maghakyan@gmail.com>
2017-08-20 17:10:27 +0200

    Wire up redux

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import React from 'react';
-import AddTodo from './components/addTodo/';
+import { connect } from 'react-redux';
+import { PropTypes } from 'prop-types';
+import AddTodo from './components/addTodo';
+import actions from './actions/';
 
-const App = () => (
+export const App = () => (
   <div>
     <h1>To-Do List</h1>
     <AddTodo submitTodo={() => {}} />
   </div>
 );
 
-export default App;
+App.propTypes = {
+  submitTodo: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = state => state.todoListApp;
+
+const mapDispatchToProps = dispatch => ({
+  submitTodo: (text) => {
+    if (text) {
+      dispatch(actions.submitTodo(text));
+    }
+  },
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
