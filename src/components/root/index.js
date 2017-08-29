f8a5e2f15ab7f23ad0815c2e5008e8b0c75c75d4
Eduard Maghakyan <edi.maghakyan@gmail.com>
2017-08-29 22:38:36 +0200

    Added router

--- src/components/root/index.js
+++ src/components/root/index.js
@@ -1,17 +1,21 @@
+/* eslint-disable react/forbid-prop-types */
 import React from 'react';
 import { Provider } from 'react-redux';
 import { PropTypes } from 'prop-types';
+import { BrowserRouter, Route } from 'react-router-dom';
 import App from '../app';
 
 const Root = ({ store }) => (
   <Provider store={store}>
-    <App />
+    <BrowserRouter>
+      <Route path="/" component={App} />
+    </BrowserRouter>
   </Provider>
 );
 
 
 Root.propTypes = {
-  store: PropTypes.object,
+  store: PropTypes.object.isRequired,
 };
 
 export default Root;
