b43650290ff7ec3525906a3bd88e870f4a0859b7
Eduard Maghakyan <edi.maghakyan@gmail.com>
2017-09-01 21:01:04 +0200

    Added logging

--- src/configureStore.js
+++ src/configureStore.js
@@ -3,6 +3,25 @@ import { createStore } from 'redux';
 import todoApp, { appState } from './reducers';
 import { loadState, saveState } from './storage';
 
+const addLoggingToDispatch = (store) => {
+  /* eslint-disable no-console */
+  const rawDispatch = store.dispatch;
+  if (!console.group) {
+    return rawDispatch;
+  }
+
+  return (action) => {
+    console.group(action.type);
+    console.log('%c prev state', 'color: gray', store.getState());
+    console.log('%c action', 'color: blue', action);
+    const returnValue = rawDispatch(action);
+    console.log('%c next state', 'color: green', store.getState());
+    console.groupEnd(action.type);
+    return returnValue;
+  };
+  /* eslint-enable no-console */
+};
+
 const configureStore = () => {
   const storedState = loadState();
 
@@ -17,6 +36,10 @@ const configureStore = () => {
     todoApp,
     persistedState,
   );
+
+  if (process.env.NODE_ENV !== 'production') {
+    store.dispatch = addLoggingToDispatch(store);
+  }
 
   store.subscribe(throttle(() => {
     saveState({
